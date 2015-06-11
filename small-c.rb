1536099ce7267636f45cb6de3051840ba7cf60c2
tattii <yuta.tatti@gmail.com>
2015-06-11 16:17:23 +0900

    add smallc lexer

---
+++ small-c.rb
@@ -0,0 +1,60 @@
+require 'strscan'
+require 'pp'
+
+class SmallC
+
+def parse(str)
+  @q = []
+  @line = 0
+  @last_newline_pos = 0
+  @last_pos = 0
+  s = StringScanner.new(str)
+
+  until s.eos?
+    case
+	when s.scan(/[ \t]+/)
+	when s.scan(/#.*?\n/)
+	when s.scan(/\n/)
+      @line++
+      @last_newline_pos = s.pos
+
+    when s.scan(/\d+/)
+	  push_token(:NUMBER, s.matched.to_i)
+	when s.scan(/int|void|if|else|while|for|return/)
+      push_token(s.matched.upcase, s.matched)
+	when s.scan(/;|,|\[|\]|\(|\)|\{|\}|\+|-|\*|\/|&|=|>|</)
+      push_token(s.matched, s.matched)
+	when s.scan(/==|!=|<=|>=|&&|\|\|/)
+      push_token(s.matched, s.matched)
+    when s.scan(/\w+/)
+	  push_token(:IDENT, s.matched)
+    end
+	@last_pos = s.pos
+  end
+  @q.push [false, '$end']
+#  do_parse
+  pp @q
+end
+
+def next_token
+  @q.shift
+end
+
+private
+def push_token(token_symbol, value)
+  @q.push [token_symbol, {
+    value: value,
+    line: @line,
+    line_pos: @last_pos - @last_newline_pos
+  }]
+end
+end
+
+parser = SmallC.new
+while true
+  puts
+  print '? '
+  str = gets.chop!
+  break if /q/i =~ str
+  parser.parse(str)
+end
